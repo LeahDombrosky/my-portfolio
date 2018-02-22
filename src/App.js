6be3a576ef33c058927d23ce5a2bb938b4d42814
Leah Dombrovsky <leahliya59@gmail.com>
2018-02-22 12:18:24 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -6,13 +6,25 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
+
+        <div className="Main-container">
+          <div className="SectionAbout">
+            <div className="Introduction">
+              <p id="Name">LEAH DOMBROVSKY</p>
+              <p id="Occupation">Web Developer</p>
+            </div>
+          </div>
+          <div className="SectionSkills" />
+          <div className="SectionProjects" />
+          <div className="SectionContact" />
+        </div>
       </div>
     );
   }
